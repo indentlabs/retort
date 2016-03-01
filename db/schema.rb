943a32d2638dc7c369a5c1002a57e8c4d95f560d
Andrew Brown <drusepth@gmail.com>
2016-03-01 17:35:04 +0000

    Include plpgsql extension in schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150924031835) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bigrams", force: :cascade do |t|
     t.string   "prior"
     t.string   "after"
