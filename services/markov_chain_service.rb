4a99ec86d2e1abcece2844ab91287edecf5e12e0
Andrew Brown <andrew@gomoment.com>
2016-05-17 14:36:32 -0500

    Speed up markov chain generation at cost of randomness for now

--- services/markov_chain_service.rb
+++ services/markov_chain_service.rb
@@ -5,17 +5,20 @@ class MarkovChainService
         # TODO: support Trigrams
         gram = Bigram.where(identifier.merge({prior: nil})).order('random()').take(1).first
         return '' if gram.nil?
+        puts "STARTING WITH ORIGIN: #{gram[:after]}"
         chain = [nil, gram[:after]]
 
         # TODO: forward and backward expansion
         # TODO: better cyclic detection
-        while (gram = Bigram.where(identifier.merge({prior: chain.last})).where.not(after: chain.last).order('random()').take(1).first)
+        while (gram = Bigram.where(identifier.merge({prior: chain.last})).where.not(after: chain.last).take(1).first)
+            puts "NEXT WORD: #{gram[:after]}"
             break if chain.length > maximum_chain_length
             break if gram[:after].nil?
             next if gram[:after].empty?
 
             chain << gram[:after]
         end
+        puts "FINISHED CHAIN: #{chain.compact}"
 
         chain.compact.join ' '
     end
